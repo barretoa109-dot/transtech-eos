f0bad8dec6f869155df773227b4972881111690f
barretoa109-dot <barretoa109@gmail.com>
2026-06-27 14:54:05 -0300

    Update route.js

--- app/app/descargar/route.js
+++ app/app/descargar/route.js
@@ -1,1 +1,18 @@
+import { redirect } from "next/navigation";
 
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+
+  const archivo = searchParams.get("archivo");
+
+  if (!archivo) {
+    return new Response("Archivo no especificado", {
+        status: 400,
+    });
+  }
+
+  const supabaseUrl =
+    `https://dirugpkamzgvyshcnsxs.supabase.co/storage/v1/object/public/EOS/${encodeURIComponent(archivo)}`;
+
+  redirect(supabaseUrl);
+}
