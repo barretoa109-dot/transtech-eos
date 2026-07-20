cfee3056cd8de4ed533ca062d8d5e616a650c974
Auugusto Barreto <galeanoaugusto05@gmail.com>
2026-07-20 15:55:47 -0300

    Corregir cliente Supabase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { supabase } from "../lib/supabase";
+import { createClient } from "../../lib/supabase/client";
 
 const servicios = [
   {
