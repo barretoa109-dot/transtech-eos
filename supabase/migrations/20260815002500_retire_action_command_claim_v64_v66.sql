873278346a7ec9531fdb7de1c6f8dae662fca0d0
barretoa109-dot <barretoa109@gmail.com>
2026-08-14 23:56:41 -0300

    chore(rc1): fold claim retirement into v70

--- supabase/migrations/20260815002500_retire_action_command_claim_v64_v66.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-revoke execute on function public.eos_claim_action_command_v64(uuid, integer)
-  from public, anon, authenticated, service_role;
-
-comment on function public.eos_claim_action_command_v64(uuid, integer) is
-  'RETIRED by RC1 v66. Runtime Worker ownership must use eos_claim_action_command_v65 with a fencing token.';
-
-commit;
